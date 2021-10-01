cb20dd976acce9fd48907f864d1c0ea6d21cb5e6
“yourGithubUsername” <your@emailadd.com>
2021-10-01 21:07:01 +0100

    add first page

---
+++ game.js
@@ -0,0 +1,62 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = []
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: "In which year was the WWW invented?",
+        choice1: "1994",
+        choice2: "1991",
+        choice3: "1988",
+        choice4: "1989",
+        answer: 4,
+    },
+    {
+        question: "What is the technical term for #?",
+        choice1: "Hashtag",
+        choice2: "Octothorp",
+        choice3: "Octothorn",
+        choice4: "Octagon",
+        answer: 2,
+    },
+    {
+        question: "Who created Python?",
+        choice1: "Brendan Eich",
+        choice2: "Guido van Rossum",
+        choice3: "Dennis Ritchie",
+        choice4: "Satoshi Nakamoto",
+        answer: 2,
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        choice1: "function myFunction()",
+        choice2: "function = myFunction()",
+        choice3: "function: myFunction()",
+        choice4: "let myFunction()",
+        answer: 1,
+    },
+    {
+        question: "Spot the odd one out",
+        choice1: "var",
+        choice2: "const",
+        choice3: "let",
+        choice4: "function",
+        answer: 4,
+    }
+    {
+        question: "What's the correct code?",
+        choice1: "var",
+        choice2: "const",
+        choice3: "let",
+        choice4: "function",
+        answer: 4,
+    }
+]
